8cdb7814ac1374df9f5c3c66ed57a8a2dc17c6e4
Qaalib Farah <[si8822fb@gmail.com]>
2019-04-13 12:58:09 -0500

    Tests for username case sensitive as well as other tests in lmn.tests.test_forms.RegistrationFormTests pass. Issue #22 solved and replaced User objects with custom user model: CustomUser. Replaced all instances of User to CustomUser in application.

---
+++ lmn/tests/test_note_views.py
@@ -0,0 +1,119 @@
+from django.test import TestCase, Client
+
+from django.urls import reverse
+from django.contrib import auth
+
+from lmn.models import Venue, Artist, Note, Show
+from django.contrib.auth.models import CustomUser
+
+import re, datetime
+from datetime import timezone
+
+
+class TestNotes(TestCase):
+    fixtures = [ 'testing_users', 'testing_artists', 'testing_venues', 'testing_shows', 'testing_notes' ]  # Have to add artists and venues because of foreign key constrains in show
+
+    def test_latest_notes(self):
+        response = self.client.get(reverse('lmn:latest_notes'))
+        expected_notes = list(Note.objects.all())
+        # Should be note 3, then 2, then 1
+        context = response.context['notes']
+        first, second, third = context[0], context[1], context[2]
+        self.assertEqual(first.pk, 3)
+        self.assertEqual(second.pk, 2)
+        self.assertEqual(third.pk, 1)
+
+
+    def test_notes_for_show_view(self):
+        # Verify correct list of notes shown for a Show, most recent first
+        # Show 1 has 2 notes with PK = 2 (most recent) and PK = 1
+        response = self.client.get(reverse('lmn:notes_for_show', kwargs={'show_pk':1}))
+        context = response.context['notes']
+        first, second = context[0], context[1]
+        self.assertEqual(first.pk, 2)
+        self.assertEqual(second.pk, 1)
+
+
+    def test_correct_templates_uses_for_notes(self):
+        response = self.client.get(reverse('lmn:latest_notes'))
+        self.assertTemplateUsed(response, 'lmn/notes/note_list.html')
+
+        response = self.client.get(reverse('lmn:note_detail', kwargs={'note_pk':1}))
+        self.assertTemplateUsed(response, 'lmn/notes/note_detail.html')
+
+        response = self.client.get(reverse('lmn:notes_for_show', kwargs={'show_pk':1}))
+        self.assertTemplateUsed(response, 'lmn/notes/note_list.html')
+
+        # Log someone in
+        self.client.force_login(CustomUser.objects.first())
+        response = self.client.get(reverse('lmn:new_note', kwargs={'show_pk':1}))
+        self.assertTemplateUsed(response, 'lmn/notes/new_note.html')
+
+
+class TestAddNotesWhenUserLoggedIn(TestCase):
+    fixtures = ['testing_users', 'testing_artists', 'testing_shows', 'testing_venues', 'testing_notes']
+
+    def setUp(self):
+        user = CustomUser.objects.first()
+        self.client.force_login(user)
+
+
+    def test_save_note_for_non_existent_show_is_error(self):
+        new_note_url = reverse('lmn:new_note', kwargs={'show_pk':100})
+        response = self.client.post(new_note_url)
+        self.assertEqual(response.status_code, 404)
+
+
+    def test_can_save_new_note_for_show_blank_data_is_error(self):
+
+        initial_note_count = Note.objects.count()
+
+        new_note_url = reverse('lmn:new_note', kwargs={'show_pk':1})
+
+        # No post params
+        response = self.client.post(new_note_url, follow=True)
+        # No note saved, should show same page
+        self.assertTemplateUsed('lmn/notes/new_note.html')
+
+        # no title
+        response = self.client.post(new_note_url, {'text':'blah blah' }, follow=True)
+        self.assertTemplateUsed('lmn/notes/new_note.html')
+
+        # no text
+        response = self.client.post(new_note_url, {'title':'blah blah' }, follow=True)
+        self.assertTemplateUsed('lmn/notes/new_note.html')
+
+        # nothing added to database
+        self.assertEqual(Note.objects.count(), initial_note_count)   # 2 test notes provided in fixture, should still be 2
+
+
+    def test_add_note_database_updated_correctly(self):
+
+        initial_note_count = Note.objects.count()
+
+        new_note_url = reverse('lmn:new_note', kwargs={'show_pk':1})
+
+        response = self.client.post(new_note_url, {'text':'ok', 'title':'blah blah' }, follow=True)
+
+        # Verify note is in database
+        new_note_query = Note.objects.filter(text='ok', title='blah blah')
+        self.assertEqual(new_note_query.count(), 1)
+
+        # And one more note in DB than before
+        self.assertEqual(Note.objects.count(), initial_note_count + 1)
+
+        # Date correct?
+        now = datetime.datetime.today()
+        posted_date = new_note_query.first().posted_date
+        self.assertEqual(now.date(), posted_date.date())  # TODO check time too
+
+
+    def test_redirect_to_note_detail_after_save(self):
+
+        initial_note_count = Note.objects.count()
+
+        new_note_url = reverse('lmn:new_note', kwargs={'show_pk':1})
+        response = self.client.post(new_note_url, {'text':'ok', 'title':'blah blah' }, follow=True)
+        new_note = Note.objects.filter(text='ok', title='blah blah').first()
+
+        self.assertRedirects(response, reverse('lmn:note_detail', kwargs={'note_pk': new_note.pk }))
