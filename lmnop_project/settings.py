33678dc1c06b8afee6e7ad6afd4eaaf60a7dfea8
Qaalib Farah <[si8822fb@gmail.com]>
2019-04-20 18:31:03 -0500

    heroku config

--- lmnop_project/settings.py
+++ lmnop_project/settings.py
@@ -78,18 +78,11 @@ WSGI_APPLICATION = 'lmnop_project.wsgi.application'
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'lmnop',
-        'USER' : 'lmnop',
-        'PASSWORD' : os.environ['LMNOP_DB_PW'],
-        'HOST' : 'localhost',
-        'PORT' : '5432',
-    }
+
 }
 
-db_from_env = dj_database_url.config(conn_max_age=600, ssl_require=True)
-DATABASES['default'].update(db_from_env)
+db_from_env = dj_database_url.config(default=config('DATABASE_URL'), conn_max_age=600, ssl_require=True)
+DATABASES['default'] = db_from_env
 
 
 # Password validation
