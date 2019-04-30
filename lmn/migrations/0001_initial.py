54410bc340174893b6e3e5365a6f1e9e75c9e241
Qaalib Farah <[si8822fb@gmail.com]>
2019-04-30 12:24:01 -0500

    heroku config

--- lmn/migrations/0001_initial.py
+++ lmn/migrations/0001_initial.py
@@ -11,7 +11,7 @@ class Migration(migrations.Migration):
     initial = True
 
     dependencies = [
-        ('auth', '0010_auto_20190413_0954'),
+        ('auth', '0009_alter_user_last_name_max_length'),
     ]
 
     operations = [
