190bf822b862c18765fe614bd040fb60e5e86ea3
Orion <orionriitters@gmail.com>
2019-05-01 00:57:45 -0500

    Put autocomplete script into its own file

---
+++ lmn/static/js/autocomplete.js
@@ -0,0 +1,12 @@
+// Code @ https://medium.com/@ninajlu/django-ajax-jquery-search-autocomplete-d4b4bf6494dd
+$(document).ready(function() {
+    $("#txtSearch").autocomplete({
+        source: "/ajax_calls/search/",
+        minLength: 2,
+        open: function() {
+            setTimeout(function() {
+                $('.ui-autocomplete').css('z-index', 99);
+            }, 0);
+        }
+    });
+});
