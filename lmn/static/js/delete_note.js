4d67ccebfe34681df9af36d7f719dfe4ef9ac02d
Qaalib Farah <[si8822fb@gmail.com]>
2019-04-30 20:02:40 -0500

    fixing delete button on js

--- lmn/static/js/delete_note.js
+++ lmn/static/js/delete_note.js
@@ -1,5 +1,5 @@
 var deleteButton = document.getElementById('delete_note_button');
-document.addEventListener('click', function (event) {
+deleteButton.addEventListener('click', function (event) {
     var confirm = confirm('Are you sure you want to delete?');
     if(!confirm){
         event.preventDefault()
