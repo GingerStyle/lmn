dc8756ba1618a06f777d572c8edb17f10f762a24
Orion <orionriitters@gmail.com>
2019-04-30 21:50:17 -0500

    Fixed issue with placement of delete_button script placement in templates.

--- lmn/static/js/delete_note.js
+++ lmn/static/js/delete_note.js
@@ -1,9 +1,9 @@
 var deleteButton = document.getElementById('delete_note_button');
 if(deleteButton){
    deleteButton.addEventListener('click', function (event) {
-    var confirm = confirm('Are you sure you want to delete?');
-    if(!confirm){
-        event.preventDefault()
+    let confirmation = confirm('Are you sure you want to delete?');
+    if(!confirmation){
+        event.preventDefault();
     }
 });
 }
