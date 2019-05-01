f86f910c0bfba6cc2f623c48e3cff93853e9b5fc
Qaalib Farah <[si8822fb@gmail.com]>
2019-04-30 19:06:13 -0500

    issue where js was not functioning correctly and the like and dislike buttons where not linking

--- lmn/static/js/delete_note.js
+++ lmn/static/js/delete_note.js
@@ -1,9 +1,7 @@
 var deleteButton = document.getElementById('delete_note_button');
-document.addEventListener()
-("#delete_note_button").click(function(event){
-    var confirm = confirm('Are you sure you want to delete?')
+document.addEventListener('click', function (event) {
+    var confirm = confirm('Are you sure you want to delete?');
     if(!confirm){
         event.preventDefault()
     }
-}
-);
+});
